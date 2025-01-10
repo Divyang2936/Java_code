617c4ab7e776ed39c34d49c66d329eb733824f52
Divyang Patel <pateldivyang2936@gmail.com>
2025-01-10 17:30:06 +0530

    add new array folder

---
+++ Array_Qusetions/Array1.java
@@ -0,0 +1,12 @@
+public class Array1 {
+
+    public static void main(String[] args) {
+        int[] marks = new int[3]; 
+        marks[0] = 97;//physics
+        marks[1] = 98;//chemistry 
+        marks[2] = 99;//maths
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
