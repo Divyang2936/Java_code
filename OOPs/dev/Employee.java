4725d163ff4150a34f196daabdcedafc06a5a6dd
Divyang Patel <pateldivyang2936@gmail.com>
2025-01-24 22:17:10 +0530

    learn about constructor

--- OOPs/dev/Employee.java
+++ OOPs/dev/Employee.java
@@ -1,13 +1,38 @@
 class Employee {
 
-    int empId = 2;
-    static int phoneNo = 0;
-    private String name = "Divyang";
-    int basicSal = 20000;
-    int incentive = 200;
+    private int empId = 2;
+    private int phoneNo = 0;
+    private String name ;
+    private int basicSal = 20000;
+    private int incentive = 200;
+
+    
 
     void countSal(){
         System.out.println(basicSal + incentive);
+    }
+
+    //getter and setter
+
+    int getphoneNo(){
+        return phoneNo;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    //constructor "creat non returen type method"
+
+    public Employee(String empName, int PhNo){
+        phoneNo = PhNo;
+        name = empName;
+    }
+
+    //create simple constructor 
+    Employee(){
+        phoneNo=222;
+        name = "Dhaval";
     }
 
 }
--- OOPs/dev/Test.java
+++ OOPs/dev/Test.java
@@ -7,17 +7,30 @@ import Mypackage2.myClass;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(Employee.phoneNo);
-        Employee enginear = new Employee();
-        System.out.println(enginear.basicSal);
+        // System.out.println(Employee.phoneNo);
+        // Employee enginear = new Employee();//defould constructor 
+        // System.out.println(enginear.basicSal);
 
-        //calling class using package from package 1
-        MyClass yClass = new MyClass();
-        yClass.print();
+        // //calling class using package from package 1
+        // MyClass yClass = new MyClass();
+        // yClass.print();
 
-        //calling class using package from package 2
-        myClass call = new myClass();
-        call.print();
+        // //calling class using package from package 2
+        // myClass call = new myClass();
+        // call.print();
+
+        //create constructor and call
+        //constructor with parameter 
+        Employee enginear = new Employee("Divyang", 21);
+
+        System.out.println(enginear.getphoneNo());//cal; the constructor 
+        System.out.println(enginear.getName());//cal; the constructor 
+
+        //simpe constructor
+        //if you not create simle constructor this will be give you error 
+        Employee engineer2 = new Employee( );
+        System.err.println(engineer2.getName());//cal; the constructor 
+        System.out.println(engineer2.getphoneNo());//cal; the constructor 
 
     }
 }
