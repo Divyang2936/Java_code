09193829992ad967a36cd03afafbd79ee2ff26af
Divyang <pateldivyang2936@gamil.com>
2025-02-24 13:05:59 +0530

    add

---
+++ leb/Practical16.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class findFactorial {
+    int factorial(int n){
+            if (n == 0 || n == 1) {
+                return 1;
+            }
+            else {
+                return n*factorial(n - 1);
+            }
+    }
+}
+
+public class Practical16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter value: ");
+        int n = sc.nextInt();
+
+        findFactorial f = new findFactorial();
+        System.out.println(f.factorial(n));
+
+    }
+}
--- leb/_24_02_2025.java
+++ leb/_24_02_2025.java
@@ -1,14 +1,14 @@
 
-class person {
-    String name ;
-    int age ;
+class person {//create person class
+    String name ;//declare instance variable name 
+    int age ;//declare instance variable age 
     void displayDetail(){
-        System.out.println("Name : "+ name + " " + ", age : "+age);
+        System.out.println("Name : " + name + " " + ", age : " + age);
     }
 }
 
-class employee extends person{
-    int salary;
+class employee extends person{//create employ class inherit peson class
+    int salary;//declare instance variable salary
     void displayEmployeeDetail(){
         displayDetail();
         System.out.println("salary is : "+salary);
@@ -17,11 +17,11 @@ class employee extends person{
 public class _24_02_2025 {
     public static void main(String[] args) {
         employee emp = new employee();
-        emp.name = "deep";
-        emp.age = 20;
-        emp.salary= 50000;
+        emp.name = "deep";//initialize value of name 
+        emp.age = 20;//initialize value of age
+        emp.salary= 50000;//initialize value of salary
 
-        //display details
+        //display details call displayEmployeeDetail
         emp.displayEmployeeDetail();
     }
 }
